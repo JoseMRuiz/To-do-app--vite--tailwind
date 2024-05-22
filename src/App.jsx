f72a4ee5e863ee9c1376df9d6a56630e289a91dd
jose m ruiz <josemruiz23@gmail.com>
2024-05-22 12:52:31 -0300

    primer dia

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,56 @@
 const app = () => {
   return(
     <>
-    <h1 className="uppercase">Todo</h1>
-    <button>
+    <div className=" bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-contain ">
+
+      <header className="container mx-auto px-4">
+        <div className="flex justify-between">
+
+        <h1 className=" uppercase">Todo</h1>
+        <button>luna</button>
+        
+        </div>
+
+        <form >
+          <input type="text" placeholder=" create a new todo.. " />
+        </form>
+
+      </header>
+
+      <main className=" container mx-auto px-4">
+
+        <article className="">
+          <button>circulo</button>
+          <p>complete online javascript course in bluweeb</p>
+          <button>exis</button>
+        </article>
+        <article className="">
+          <button>circulo</button>
+          <p>complete online javascript course in bluweeb</p>
+          <button>exis</button>
+        </article>
+        <article className="">
+          <button>circulo</button>
+          <p>complete online javascript course in bluweeb</p>
+          <button>exis</button>
+        </article>
+        <section>
+          <span>5 items left...</span>
+          <button>clear completed</button>
+        </section>
+
+      </main>
+
+      <section className="container mx-auto px-4">
+        <button> All </button>
+        <button> Active </button>
+        <button> Completed </button>
+      </section>
+
+      <p className="text-center">Drag an drop to reorder list</p>
       
-    </button>
+    </div>
+    
 
     
     
@@ -12,3 +58,4 @@ const app = () => {
   )
 }
 
+export default app;
