e63b1e09ac3dd0ab9047436257a462ca42963e28
jose m ruiz <josemruiz23@gmail.com>
2024-05-24 09:17:03 -0300

    dia 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import CrossIcon from "./components/CrossIcon";
+import MoonIcon from "./components/MoonIcon";
 
 const app = () => {
   return(
@@ -10,7 +11,7 @@ const app = () => {
         <div className="flex justify-between">
 
         <h1 className=" text-white text-3xl font-semibold tracking-[0.3em]  uppercase">Todo</h1>
-        <button>luna</button>
+        <button><MoonIcon className="fill-red-500" /></button>
         
         </div>
 
@@ -23,43 +24,47 @@ const app = () => {
 
       <main className="mt-8 container mx-auto px-4">
 
-        <div className=" rounded-md  bg-white">
+        <div className=" rounded-md [&>article]:p-4 bg-white">
 
-          <article className="  flex gap-4 py-4 px-4 border-b-gray-400 border-b ">
+          <article className="  flex gap-4   border-b-gray-400 border-b ">
             <button className="rounded-full border-2 h-5 w-5 flex-none inline-block"></button>
             <p className=" text-gray-600 grow ">complete online javascript course in bluweeb</p>
             <button className=" flex-none"><CrossIcon/></button>
           </article>
 
 
-          <article className="  flex gap-4 py-4 px-4 border-b-gray-400 border-b ">
+          <article className="  flex gap-4   border-b-gray-400 border-b ">
             <button className="rounded-full border-2 h-5 w-5 flex-none inline-block"></button>
             <p className=" text-gray-600 grow ">complete online javascript course in bluweeb</p>
             <button className=" flex-none"><CrossIcon/></button>
           </article>
 
 
-          <article className="  flex gap-4 py-4 px-4 border-b-gray-400 border-b ">
+          <article className="  flex gap-4  border-b-gray-400 border-b ">
             <button className="rounded-full border-2 h-5 w-5 flex-none inline-block"></button>
             <p className=" text-gray-600 grow ">complete online javascript course in bluweeb</p>
             <button className=" flex-none"><CrossIcon/></button>
           </article>
 
 
-          <section className="py-4 px-4">
+          <section className="flex justify-between py-4 px-4">
             <span className=" text-gray-400">5 items left...</span>
             <button className=" text-gray-400">clear completed</button>
           </section>
         </div>
       </main>
 
-      <section className="container mx-auto px-4">
-        <button> All </button>
-        <button> Active </button>
-        <button> Completed </button>
+      <section className="container  mx-auto mt-8 px-4">
+
+        <div className=" flex justify-center bg-white p-4 rounded-md gap-4 ">
+          <button className=" text-blue-600"> All </button>
+          <button className=" hover:text-blue-600"> Active </button>
+          <button className=" hover:text-blue-600"> Completed </button>
+        </div>
+        
       </section>
 
-      <p className="text-center">Drag an drop to reorder list</p>
+      <p className="text-center mt-8">Drag an drop to reorder list</p>
       
     </div>
     
