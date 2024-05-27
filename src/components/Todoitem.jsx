d40a510f9d53b12f348c4e406cc14d1419f8d7ff
jose m ruiz <josemruiz23@gmail.com>
2024-05-27 10:32:29 -0300

    ca,bios en todo item

--- src/components/Todoitem.jsx
+++ src/components/Todoitem.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import CrossIcon from './CrossIcon'
+import CrossIcon from './CrossIcon';
 
-const Todoitem = () => {
+
+
+const TodoItem = ({todo}) => {
+
+    const {id,title,completed} = todo;
+
     return (
-        <article className="  flex gap-4  border-b-gray-400 border-b ">
+            <article className="  flex gap-4   border-b-gray-400 border-b ">
                 <button className="rounded-full border-2 h-5 w-5 flex-none inline-block"></button>
-                <p className=" text-gray-600 grow ">complete online javascript course in bluweeb</p>
+                <p className=" text-gray-600 grow ">{title}</p>
                 <button className=" flex-none"><CrossIcon/></button>
             </article>
     )
 }
-
-export default Todoitem;
